c4d63b1a9f3d8679e9369e90c688cbea54d95b58
Daniel Mack <daniel@zonque.org>
2016-09-07 23:15:45 +0200

    model all the code around a class

--- HomeControlBot.rb
+++ HomeControlBot.rb
@@ -4,126 +4,94 @@
 
 require 'yaml'
 require 'telegram/bot'
+require 'thread'
 require 'net/ping'
 
 include Net
 
-config = YAML.load(File.read("HomeControlBot.yml"))
-chats = IO.readlines("HomeControlBot.chatids").map { |s| s.to_i } rescue []
+class HomeControlBot
 
-pingStatus = {}
-pingHosts = config["ping_hosts"]
-pingHosts.each { |ph| pingStatus[ph] = false }
+  def initialize()
+    @mutex = Mutex.new
 
-def countFiles(dir)
-  d = Dir.new(dir)
-  count = 0
-  d.each { |f| count += 1 }
+    @config = YAML.load(File.read("HomeControlBot.yml"))
+    @chats = IO.readlines("HomeControlBot.chatids").map { |s| s.to_i } rescue []
+    @chats.uniq!
 
-  count
-end
+    @pingStatus = {}
+    @pingHosts = @config["ping_hosts"]
+    @pingHosts.each { |ph| @pingStatus[ph] = false }
 
-monitorDirs = config["monitor_dirs"].map do |dir|
-  { dir: dir, count: countFiles(dir) }
-end
-
-broadcasts = Queue.new
-
-def writeChatIDs(chats)
-  chats.uniq!
-
-  File.open("HomeControlBot.chatids", 'w') do |f|
-    chats.each do |c|
-      f.write("#{c}\n")
+    @monitorDirs = @config["monitor_dirs"].map do |dir|
+      { dir: dir, count: countFiles(dir) }
     end
-  end
-end
 
-def startMotion
-  system("systemctl start motion")
-end
+    @broadcasts = Queue.new
 
-def stopMotion
-  system("systemctl stop motion")
-end
-
-$started = false
-$timeout = 0
-
-def checkPings
-  any = false
-
-  pingStatus.each do |ph, status|
-    any = true if status == true
+    @motionStarted = false
+    @pingTimeout = 0
   end
 
-  if any
-    $timeout = 0
-  else
-    $timeout += 1
+  def broadcast(msg)
+    @broadcasts.push(msg)
   end
 
-  if $timeout > 3 && !$started
-    broadcasts.push("Hey, have your mobiles all left the flat? Enabling the camera now!")
-    startMotion
-    $started = true
+  def countFiles(dir)
+    d = Dir.new(dir)
+    count = 0
+    d.each { |f| count += 1 }
+
+    count
   end
 
-  if $timeout == 0 && $started
-    broadcasts.push("Ah, there you are! Camera is off again, no worries!")
-    stopMotion
-    $started = false
-  end
-end
+  def writeChatIDs
+    @chats.uniq!
 
-Telegram::Bot::Client.run(config["telegram_token"]) do |bot|
-  threads = []
-
-  threads << Thread.new do
-    loop do
-      msg = broadcasts.pop
-
-      chats.uniq.each do |chat|
-        bot.api.send_message(chat_id: chat, text: msg)
-      end
-
-      puts "Broadcasting '#{msg}' to #{chats.count} channels"
-    end
-  end
-
-  pingHosts.each do |ph|
-    threads << Thread.new do
-      p = Net::Ping::ICMP.new(ph, nil, 2)
-
-      loop do
-        pingStatus[ph] = p.ping?
-        checkPings
-        sleep pingStatus[ph] ? 10 : 1
+    File.open("HomeControlBot.chatids", 'w') do |f|
+      @chats.each do |c|
+        f.write("#{c}\n")
       end
     end
   end
 
-  threads << Thread.new do
-    loop do
-      monitorDirs.each do |d|
-        count = countFiles(d[:dir])
-        if count != d[:count]
-          broadcasts.push("Alert: #{count - d[:count]} new file(s) in #{d[:dir]}. Go check.")
-        end
+  def startMotion
+    system("systemctl start motion")
+  end
 
-        d[:count] = count
-      end
+  def stopMotion
+    system("systemctl stop motion")
+  end
 
-      sleep 60
+  def checkPings
+    any = false
+
+    @pingStatus.each do |ph, status|
+      any = true if status == true
+    end
+
+    if any
+      @pingTimeout = 0
+    else
+      @pingTimeout += 1
+    end
+
+    if @pingTimeout > 3 && !@motionStarted
+      broadcast("Hey, have your mobiles all left the flat? Enabling the camera now!")
+      startMotion
+      @motionStarted = true
+    end
+
+    if @pingTimeout == 0 && @motionStarted
+      broadcast("Ah, there you are! Camera is off again, no worries!")
+      stopMotion
+      @motionStarted = false
     end
   end
 
-  puts "Started. Listening for messages."
-
-  bot.listen do |message|
-    unless config["telegram_allowed"].include? message.from.id
+  def dispatchMessage(bot, message)
+    unless @config["telegram_allowed"].include? message.from.id
       bot.api.send_message(chat_id: message.chat.id, text: "Huh. Who are you? I'm not talking to strangers.")
-      next
+      return
     end
 
     case message.text
@@ -148,10 +116,68 @@ Telegram::Bot::Client.run(config["telegram_token"]) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: `mount`)
     when '/pingstatus'
       s = "Ping status:\n"
-      pingStatus.each { |ph, status| s += "#{ph} is #{status ? 'up' : 'down'}\n" }
+      @pingStatus.each { |ph, status| s += "#{ph} is #{status ? 'up' : 'down'}\n" }
+      s += "Timeout = #{@pingTimeout}"
       bot.api.send_message(chat_id: message.chat.id, text: s)
     end
   end
 
-  threads.each(&:kill)
+  def run
+    Telegram::Bot::Client.run(@config["telegram_token"]) do |bot|
+      threads = []
+
+      threads << Thread.new do
+        loop do
+          msg = @broadcasts.pop
+          #puts "Broadcasting '#{msg}' to #{@chats.count} channels"
+
+          @mutex.synchronize do
+            @chats.uniq.each do |chat|
+              bot.api.send_message(chat_id: chat, text: msg)
+            end
+          end
+        end
+      end
+
+      @pingHosts.each do |ph|
+        threads << Thread.new do
+          p = Net::Ping::ICMP.new(ph, nil, 2)
+
+          loop do
+            @mutex.synchronize do
+              @pingStatus[ph] = p.ping?
+              checkPings
+            end
+            sleep @pingStatus[ph] ? 10 : 1
+          end
+        end
+      end
+
+      threads << Thread.new do
+        loop do
+          @monitorDirs.each do |d|
+            count = countFiles(d[:dir])
+            if count != d[:count]
+              broadcast("Alert: #{count - d[:count]} new file(s) in #{d[:dir]}. Go check.")
+            end
+
+            d[:count] = count
+          end
+
+          sleep 60
+        end
+      end
+
+      puts "Started. Listening for messages."
+
+      bot.listen do |message|
+        dispatchMessage(bot, message)
+      end
+
+      threads.each(&:kill)
+    end
+  end
 end
+
+hcb = HomeControlBot.new
+hcb.run
