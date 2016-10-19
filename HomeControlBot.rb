926aba4087511ad8c1c7df0eb48e0dc5b6e54140
Daniel Mack <daniel@zonque.org>
2016-10-19 18:27:36 +0200

    add support for dimmer control

--- HomeControlBot.rb
+++ HomeControlBot.rb
@@ -6,8 +6,32 @@ require 'yaml'
 require 'telegram/bot'
 require 'thread'
 require 'net/ping'
+require 'socket'
 
 include Net
+
+class CycleChannel
+  def initialize(step, start = 0, min = 0, max = 256)
+    @step, @val, @min, @max = step, start, min, max
+    @dir = 1.0
+  end
+
+  def next
+    @val += @step * @dir
+
+    if (@val > @max) then
+      @val = 2 * @max - @val
+      @dir = -1.0
+    end
+
+    if (@val < @min) then
+      @val *= -1.0
+      @dir = 1.0
+    end
+
+    @val.to_i
+  end
+end
 
 EMOJI_CAMERA = "\u{1F4F7}"
 
@@ -32,6 +56,8 @@ class HomeControlBot
 
     @broadcasts = Queue.new
     @pingTimeout = 0
+
+    @lightMode = ''
   end
 
   def parseTime(s)
@@ -120,6 +146,7 @@ class HomeControlBot
 
       threads << Thread.new do
         motionStarted = false
+        prevLightMode = nil
 
         loop do
           @mutex.synchronize do
@@ -131,13 +158,21 @@ class HomeControlBot
           end
 
           if @pingTimeout > @config["ping_timeout"] && !motionStarted
+            prevLightMode = @lightMode
+            @lightMode = 'off'
             broadcast("Ping timeout. #{EMOJI_CAMERA} on!")
             startMotion
             motionStarted = true
           end
 
           if @pingTimeout == 0 && motionStarted
-            broadcast("#{EMOJI_CAMERA} off")
+            s = "#{EMOJI_CAMERA} off. "
+            if prevLightMode and (Time.now.hour > 18 or Time.now.hour < 6)
+              @lightMode = prevLightMode
+              s += "Lights back to #{prevLightMode}."
+            end
+
+            broadcast(s)
             stopMotion
             motionStarted = false
           end
@@ -156,15 +191,84 @@ class HomeControlBot
             if new_files.any?
               broadcast("Alert: #{new_files.count} new file(s) in #{d[:dir]}. Go check.")
 
-              new_files.each do |nf|
+              files.each do |nf|
                 next unless /\.avi$/.match(nf)
-                #puts "Broadcasting #{nf}"
+                puts "Broadcasting #{nf}"
                 broadcast_video(nf)
               end
             end
           end
 
           sleep 60
+        end
+      end
+
+      threads << Thread.new do
+        cycleR = CycleChannel.new(0.1)
+        cycleG = CycleChannel.new(0.2)
+        cycleB = CycleChannel.new(0.3)
+        cycleW = CycleChannel.new(0.4)
+
+        socket = UDPSocket.new
+        payload = Array.new(513, 0)
+        strobeState = 0
+
+        fixColors = {
+          off:    { r: 0,   g: 0,   b: 0,   w: 0   },
+          red:    { r: 255, g: 0,   b: 0,   w: 0   },
+          green:  { r: 0,   g: 255, b: 0,   w: 0   },
+          blue:   { r: 0,   g: 0,   b: 255, w: 0   },
+          yellow: { r: 255, g: 255, b: 0,   w: 0   },
+          orange: { r: 255, g: 127, b: 0,   w: 10  },
+          pink:   { r: 255, g: 0,   b: 255, w: 0   },
+        }
+
+        loop do
+          payload.map! { 0 }
+          delay = nil
+
+          case @lightMode
+          when 'off'
+            delay = 1
+
+          when 'cycle'
+            payload[4] = 255
+            payload[5] = cycleR.next
+            payload[6] = cycleG.next
+            payload[7] = cycleB.next
+            payload[8] = 0 #cycleW.next
+
+            delay = 0.02
+
+          when 'strobe'
+            strobeState ^= 1
+
+            payload[4] = strobeState * 255
+            payload[5] = strobeState * 255
+            payload[6] = strobeState * 255
+            payload[7] = strobeState * 255
+            payload[8] = strobeState * 255
+
+            delay = strobeState ? 0.05 : 0.7
+
+          else
+            fix = fixColors[@lightMode.to_sym]
+            if fix
+              payload[4] = 255
+              payload[5] = fix[:r]
+              payload[6] = fix[:g]
+              payload[7] = fix[:b]
+              payload[8] = fix[:w]
+              delay = 1
+            end
+          end
+
+          if delay
+            socket.send(payload.pack("C*"), 0, "127.0.0.1", 4444)
+            sleep delay
+          else
+            sleep 1.0
+          end
         end
       end
 
@@ -184,6 +288,12 @@ class HomeControlBot
         if @stickers.keys.include? cmd
           sticker = @stickers[cmd].shuffle.first
           bot.api.send_sticker(chat_id: message.chat.id, sticker: sticker)
+          next
+        end
+
+        if cmd.start_with?("light_")
+          @lightMode = cmd[6..-1]
+          bot.api.send_message(chat_id: message.chat.id, text: "Setting light mode to '#{@lightMode}'")
           next
         end
 
